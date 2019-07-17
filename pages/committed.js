42e26e4e28177237c989b5bf4ac2688e38570aa3
Frank <fgunsch.webdev@gmail.com>
2019-07-17 00:06:37 -0400

    Add higher quality favicon settings

--- pages/committed.js
+++ pages/committed.js
@@ -11,7 +11,13 @@ const major = () => (
       <meta name='description' content='Page for major projects.' />
       <meta name='author' content='Frank Gunsch' />
       <link href='https://fonts.googleapis.com/css?family=Anaheim&display=swap' rel='stylesheet' />
-      <link rel='shortcut icon' type='image/x-icon' href='../static/focus.ico' />
+      <link rel='apple-touch-icon' sizes='180x180' href='../static/apple-touch-icon.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='../static/favicon-32x32.png' />
+      <link rel='icon' type='image/png' sizes='16x16' href='../static/favicon-16x16.png' />
+      <link rel='manifest' href='../static/site.webmanifest' />
+      <link rel='mask-icon' href='../static/safari-pinned-tab.svg' color='#5bbad5' />
+      <meta name='msapplication-TileColor' content='#da532c' />
+      <meta name='theme-color' content='#ffffff' />
     </Head>
     {/* major */}
     <NavMenu />
