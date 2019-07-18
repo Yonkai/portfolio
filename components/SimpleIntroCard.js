b70cc07ca06bae4d0cb11fb256fc7c13c1b2f6ce
Frank <fgunsch.webdev@gmail.com>
2019-07-17 20:54:33 -0400

    Add icons/hero picture

--- components/SimpleIntroCard.js
+++ components/SimpleIntroCard.js
@@ -2,6 +2,14 @@ const SimpleIntroCard = () => {
   return (
     <div className='faking-normal-flow'>
       <h1>Hello! I'm Frank, A <span>full stack web developer</span> from Poughkeepsie, New York.</h1>
+
+      <picture>
+        <img
+          src='../static/hero-1-srcset/hero-1-cropped_r7eag0_c_scale,w_200.png'
+          alt='Picture of me.'
+          height='165px'
+          width='165px' />
+      </picture>
       <style jsx>{`
               .faking-normal-flow{
                 box-sizing:border-box;
@@ -13,13 +21,28 @@ const SimpleIntroCard = () => {
                 border-right:solid black 20px;
               }
               h1{
-                margin-top:120px;
+                margin-top:90px;
                 font-size:30px;
                 color:rgb(250,250,250);
                 text-align:center;
               }
               span{
                 background: linear-gradient(90deg, rgba(252,176,69,.5) 0%, rgba(252,176,29,.4) 25%, rgba(253,160,49,.4) 50%, rgba(203,140,69,.5) 76%, rgba(252,176,69,.4) 100%);
+              }
+              picture{
+                margin-top:-10px;
+                display:flex;
+                justify-content:center;
+              }
+              img{
+                border-radius:100%;
+                filter: opacity(75%);
+              }
+
+              @media (max-width: 370px) {
+                picture{
+                  display:none;
+                }
               }
       `}</style>
     </div>
